8bff69440c43678867b822a7b434fe91fa9774c0
joaogabrielsantosD <joao.donato@ufpe.br>
2026-06-09 11:32:32 -0300

    feat: add LCD controller and logic modules, update top-level connections for LCD integration

--- src/top.vhd
+++ src/top.vhd
@@ -3,6 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.fsm_pkg.all;
 use ieee.i2c_pkg.all;
 use ieee.seven_seg_pkg.all;
+use ieee.lcd_vhdl_package.all;
 
 entity top is
 	port (
@@ -15,6 +16,11 @@ entity top is
 		dig : out std_logic_vector(3 downto 0);
 		seg : out std_logic_vector(7 downto 0);
 		
+		lcd_rs : out std_logic;
+		lcd_rw : out std_logic;
+		lcd_en : out std_logic;
+		lcd_d  : out std_logic_vector(7 downto 0);
+		
 		key : in std_logic_vector(3 downto 0);
 		
 		led : out std_logic_vector(3 downto 0)
@@ -23,6 +29,18 @@ end entity;
 
 architecture top_level of top is
 	signal temp_data : std_logic_vector(15 downto 0);
+	
+	signal music_selection : std_logic_vector(1 downto 0);
+	signal music_fsm_state : std_logic_vector(1 downto 0);
+	
+	signal min  : std_logic_vector(3 downto 0);
+	signal dsec : std_logic_vector(3 downto 0);
+	signal usec : std_logic_vector(3 downto 0);
+	signal msec : std_logic_vector(3 downto 0);
+	
+	signal lcd_busy : std_logic;
+	signal lcd_enable_int : std_logic;
+	signal lcd_bus_int    : std_logic_vector(9 downto 0);
 
 begin
 	fsm_states : fsm port map (
@@ -30,7 +48,9 @@ begin
 		rst_n => rst_n,
 		key => key(0),
 		button => key(3 downto 1),
-		debug => led
+		debug => led,
+		music_sel => music_selection,
+		music_state => music_fsm_state
 	);
 
 	temperature : i2c port map (
@@ -46,6 +66,31 @@ begin
 		data_in => temp_data,
 		dig => dig,
 		seg => seg
+	);
+	
+	ctrl_lcd : lcd_logic port map (
+		clk => clock,
+		lcd_busy => lcd_busy,
+		musica => music_selection,
+		minuto => min,
+		dez_seg => dsec,
+		uni_seg => usec,
+		dec_seg => msec,
+		fsm_state => music_fsm_state,
+		lcd_e => lcd_enable_int,
+		lcd_bar => lcd_bus_int
+	);
+	
+	lcd_hw : lcd_controller port map (
+		clk => clock,
+		reset_n => rst_n,
+		lcd_enable => lcd_enable_int,
+		lcd_bus => lcd_bus_int,
+		busy => lcd_busy,
+		rw => lcd_rw,
+		rs => lcd_rs,
+		e => lcd_en,
+		lcd_data => lcd_d
 	);
 	
 	--led(0) <= temp_data(8);
